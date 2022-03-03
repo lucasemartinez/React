6fa3f2e2d89a313213cb8ff3b231bf1ac2304214
lucasemartinez <luqi_00@hotmail.com>
2022-03-03 00:59:38 -0300

    ItemListContainer Update

--- tera/src/components/Container/ItemListContainer.jsx
+++ tera/src/components/Container/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card'
 
 export default function ItemListContainer({ welcome }) {
   return (
-      <Card style={ {width: '50%', margin: '0 auto'}}>
+      <Card style={ {width: '50%', margin: '10rem auto', backgroundColor: '#b8b8b8'}}>
         <Card.Body>
             <Card.Title>
                 <h1>{welcome}</h1>
@@ -12,7 +12,7 @@ export default function ItemListContainer({ welcome }) {
                 <p>La empresa lider en venta de Drones en Argentina!</p>
             </Card.Text>
         </Card.Body>
-        <Card.Footer style={{ background: 'white'}}>
+        <Card.Footer style={{ background: '#e3e3e3'}}>
             <p>Compras con envio gratis</p>
         </Card.Footer>
       </Card>
